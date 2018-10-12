023841da32d13431d5ab62c3e8b698c276d473cb
Froilan Irizarry <froilan@froilanirizarry.me>
2018-10-11 21:19:03 -0400

    Minor refactor - Added persist to nock interceptor - Made all headers in nock lowercase - Changed tests to use async/await

--- tests/github/github.test.js
+++ tests/github/github.test.js
@@ -28,6 +28,7 @@ describe('Test Github Integration', () => {
     const user = 'froi';
 
     nock('https://api.github.com')
+      .persist()
       .get('/rate_limit').reply(200, rateLimitResponse)
       .get(`/repos/${owner}/${repo}/readme`)
       .reply(200, { data: getReadmeResponse }, {
@@ -37,16 +38,16 @@ describe('Test Github Integration', () => {
       })
       .get(`/repos/${owner}/${repo}/languages`)
       .reply(200, getRepoLanguagesResponse, {
-        "X-RateLimit-Limit": 60,
-        "X-RateLimit-Remaining": 59,
-        "X-RateLimit-Reset": 123456
+        "x-ratelimit-limit": 60,
+        "x-ratelimit-remaining": 59,
+        "x-ratelimit-reset": 123456
       })
       .get(`/repos/${owner}/${repo}/contributors`)
       .query({ anon: false, per_page: 10, page: 0 })
       .reply(200, getRepoContributorsResponse, {
-        "X-RateLimit-Limit": 60,
-        "X-RateLimit-Remaining": 59,
-        "X-RateLimit-Reset": 123456
+        "x-ratelimit-limit": 60,
+        "x-ratelimit-remaining": 59,
+        "x-ratelimit-reset": 123456
       })
       .get(`/users/${user}`)
       .reply(200, getUsersResponse, {
@@ -72,60 +73,47 @@ describe('Test Github Integration', () => {
   });
 
   describe('get the readme for a repo', () => {
-    it('should return the repo readme text', () => {
-      return getRepoReadme('gsa', 'code-gov-api', github)
-        .then(data => expect(data.readme).to.deep.equal({ data: 'Mock Readme.' }));
+    it('should return the repo readme text', async () => {
+      const data = await getRepoReadme('gsa', 'code-gov-api', github);
+      expect(data.readme).to.deep.equal({ data: 'Mock Readme.' });
     });
   });
 
   describe('get the languages for a repo', () => {
-    it('should return the repo languages', () => {
-      return getRepoLanguages('gsa', 'code-gov-api', github)
-        .then(data => expect(data.languages).includes('JavaScript'));
+    it('should return the repo languages', async () => {
+      const data = await getRepoLanguages('gsa', 'code-gov-api', github);
+      expect(data.languages).includes('JavaScript');
     });
   });
 
   describe('get the contributors for a repo', () => {
-    it('should return the contributors array', () => {
-      return getRepoContributors({owner: 'gsa', repo: 'code-gov-api', client: github })
-        .then(data => {
-          const { contributors } = data;
-          expect(contributors[0].gh_profile).to.be.equal('https://github.com/froi');
-        });
+    it('should return the contributors array', async () => {
+      const data = await getRepoContributors({owner: 'gsa', repo: 'code-gov-api', client: github })
+      const { contributors } = data;
+
+      expect(contributors[0].gh_profile).to.be.equal('https://github.com/froi');
     });
   });
 
   describe('get the issues for a repo', () => {
-    it('should return an array of issues', () => {
-      return getRepoIssues({ owner: 'gsa', repo: 'code-gov-api', per_page:10, page: 0, client: github})
-        .then(data => {
-          const issues = data.issues;
-          expect(data).to.haveOwnProperty('issues');
-          expect(issues).to.be.instanceOf(Array);
-          expect(issues[0]).to.haveOwnProperty('issue_url');
-          expect(issues[0].issue_url).to.be.equal('https://github.com/GSA/code-gov-api/issues/243');
-        });
+    it('should return an array of issues', async () => {
+      const data = await getRepoIssues({ owner: 'gsa', repo: 'code-gov-api', per_page:10, page: 0, client: github});
+      const issues = data.issues;
+
+      expect(data).to.haveOwnProperty('issues');
+      expect(issues).to.be.instanceOf(Array);
+      expect(issues[0]).to.haveOwnProperty('issue_url');
+      expect(issues[0].issue_url).to.be.equal('https://github.com/GSA/code-gov-api/issues/243');
     });
   });
 
   describe('get data for a repo', () => {
-    it('should return repo data object', () => {
-      return getRepoData('gsa', 'code-gov-api', github)
-        .then(data => {
-          expect(data).to.haveOwnProperty('repo');
-          expect(data.repo).to.haveOwnProperty('full_name');
-          expect(data.repo.full_name).to.be.equal('GSA/code-gov-api');
-        });
-    });
-  });
+    it('should return repo data object', async () => {
+      const data = await getRepoData('gsa', 'code-gov-api', github);
 
-  describe('get data for a repo', () => {
-    it('should return repo data object', () => {
-      return getData('gsa', 'code-gov-api', github)
-        .then(data => {
-          expect(data).to.haveOwnProperty('full_name');
-          expect(data.full_name).to.be.equal('GSA/code-gov-api');
-        });
+      expect(data).to.haveOwnProperty('repo');
+      expect(data.repo).to.haveOwnProperty('full_name');
+      expect(data.repo.full_name).to.be.equal('GSA/code-gov-api');
     });
   });
 });
